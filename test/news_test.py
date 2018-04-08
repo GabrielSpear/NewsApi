eb2bac9407c5e6b287504ab5ccb76ca83e2b88c9
Gabriel <gabrieldvjspear@gmail.com>
2018-04-08 22:41:01 +0300

    function runs before any test

--- test/news_test.py
+++ test/news_test.py
@@ -5,3 +5,9 @@ class NewsTest(unittest.TestCase):
     '''
     test behaviour of news class
     '''
+
+    def setUp(self):
+        '''
+        will run before every test
+        '''
+        self.new_articles = Articles('id','name','author','title','description','url','urlToImage','publishedAt')
