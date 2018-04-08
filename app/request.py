76675168b2bfedba8ec2f66e139aff10d311fa72
Gabriel <gabrieldvjspear@gmail.com>
2018-04-08 23:04:56 +0300

    adding country source

--- app/request.py
+++ app/request.py
@@ -14,3 +14,24 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_KEY_URL']
     source_url = app.config['NEWS_API_SOURCES_URL']
+
+
+
+
+def get_news(country,category):
+    '''
+    function of getting json response ro url
+    '''
+    get_news_url = base_url.format(country,category,api_key)
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_result = None
+
+
+        if get_news_response['sources']:
+            news_result_list = get_news_response['sources']
+            news_result = process_results(news_result_list)
+
+    return news_result
