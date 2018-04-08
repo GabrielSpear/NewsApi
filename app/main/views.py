d340b952c34198096ec3de93a4f96d9e3399082b
Gabriel <gabrieldvjspear@gmail.com>
2018-04-08 22:00:45 +0300

    creating and passing on variables

--- app/main/views.py
+++ app/main/views.py
@@ -12,3 +12,18 @@ def index():
     '''
     message = 'hello world'
     title = 'One of the best News Website in the world'
+
+
+
+
+
+
+    @main.route('/news/<id>')
+def news(id):
+    """
+    View articles page that returns the news article from a highlight
+    """
+    news_args = get_details(id)
+    highlight_args = 'Route Working!!'
+    # name = f'{results_list}'
+    return render_template('news.html',highlight_param=highlight_args,news=news_args)
