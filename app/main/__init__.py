212b9f061e65d94aff332fe75a8667e1275312c8
Gabriel <gabrieldvjspear@gmail.com>
2018-04-08 21:40:06 +0300

    creating app configurations

---
+++ app/main/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+
+# Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+
+
+
+# setting config
+    from .request import configure_request
+    configure_request(app)
+
+    return app
---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
